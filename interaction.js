ef5c1862f9e7012ad5fbac511daa0ad309bd3561
sabrina belkhodja <sbelkhodja@e010m10.istic.univ-rennes1.fr>
2024-09-26 09:37:45 +0200

    test

--- interaction.js
+++ interaction.js
@@ -3,10 +3,45 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
+  this.positionInitialeX = 0;
+  this.positionInitialeY = 0;
+  this.positionFinaleX = 0;
+  this.positionFinaleY= 0;
+  this.clicked = false;
 
 	// Developper les 3 fonctions gérant les événements
 
+  DnD.prototype.Drag = function(evt){
+    var position = getMousePosition(canvas,evt);
+    this.positionInitialeX  = position.x;
+    this.positionInitialeY = position.y;
+    this.clicked = true;
+    console.log('drag x : ',this.positionInitialeX ,' y : ',this.positionInitialeY)
+  }
+
+  DnD.prototype.Move = function(evt){
+
+  }
+
+  DnD.prototype.Drop = function(evt){
+    if(this.clicked){
+      var position = getMousePosition(canvas,evt);
+      this.positionFinaleX = position.x;
+      this.positionFinaleY = position.y;
+      console.log('drop x : ',this.positionFinaleX ,' y : ',this.positionFinaleY)
+    }
+  
+
+  }
+
 	// Associer les fonctions précédentes aux évènements du canvas.
+
+  canvas.addEventListener('mousedown', this.Drag, false);
+
+  canvas.addEventListener('mousemove', this.Move, false);
+  
+  canvas.addEventListener('mouseup', this.Drop, false);
+
 };
 
 
